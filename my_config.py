184ba32ebdc36306b899bf5ea55a379b78dee7a0
rggassner <rafael.gassner@gmail.com>
2025-09-07 08:38:38 -0300

    Create my_config.py

---
+++ my_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+user = 'api'
+password = 'api'
+url = '1.1.5.1'
+api_wait_time=.1
+page_size=500
+
+SOURCE_DOM='SOURCE_CMA'
+SOURCE_PROFILE='SOURCE_PROFILE'
+DESTINATION_PROFILE='Basic'
+PROTECTIONS_LIMIT=32764
+MAX_PUBLISH=100
+PROTECTION_FILE='protections.json'
